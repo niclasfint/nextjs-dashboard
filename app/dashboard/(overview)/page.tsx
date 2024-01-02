14c381fb6c53b8cbd3e86d74b361760f41f42e89
Niclas Svensson <niclas.svensson@briox.se>
2024-01-02 16:27:16 +0100

    done

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -11,6 +11,12 @@ import {
   CardsSkeleton,
 } from '@/app/ui/skeletons';
  
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Home',
+};
+
 export default async function Page() {
 
   //const latestInvoices = await fetchLatestInvoices();
